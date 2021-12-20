019b963dd635db6543e1313cde02e3c363a71b6a
github akito-10 <akt.fukuda@gmail.com>
2021-12-20 22:10:43 +0900

    APIエンドポイントの記述を変更

--- src/libs/baseUrl.ts
+++ src/libs/baseUrl.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.API_URL || "http://localhost:8000";
+export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:8000" : process.env.API_URL;
